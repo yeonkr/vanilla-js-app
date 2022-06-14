5185f2bff7f462a23d73cd129454c11726ba62de
yeonkr <1714882@sookmyung.ac.kr>
2022-06-15 00:34:40 +0900

    7.8

--- js/todo.js
+++ js/todo.js
@@ -13,13 +13,15 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement // 버튼의 부모에 접근
+    console.log(li.id)
     li.remove();
 }
 
 function paintToDo(newTodo) { // handleToDoSubmit 함수가 이 paintToDo를 사용할 것.
     const li = document.createElement("li") 
+    li.id = newTodo.id;
     const span = document.createElement("span")
-    span.innerText = newTodo; // span의 텍스트는 handleToDoSubmit에서 온 newTodo 텍스트가 됨.
+    span.innerText = newTodo.text; // span의 텍스트는 handleToDoSubmit에서 온 newTodo 텍스트가 됨.
     const button = document.createElement("button")
     button.innerText = "❌"
     button.addEventListener("click", deleteToDo) // click --> deleteToDo함수가 실행
@@ -32,8 +34,12 @@ function handleToDoSubmit(event) {
     event.preventDefault(); // 기본값을 없애줌. 
     const newTodo = toDoInput.value; // text상자에 사용자가 입력한 텍스트를 저장해야 나중에 ul태그 안에 사용할 수 있기 때문에 이를 정의하여 newTodo라는 값에 저장되게 만듦
     toDoInput.value = ""; // text 상자에 글을 작성하고 enter를 눌렀을 때 그 글이 초기화 되게 만들기 위해 value를 빈 텍스트로 한다.
-    toDos.push(newTodo) // newTodo를 toDos array에 push한 다음
-    paintToDo(newTodo); // 화면에 그 toDo를 그려줌
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(), // id로 각각의 li item 구별 
+    };
+    toDos.push(newTodoObj) // newTodoObj를 toDos array에 push한 다음
+    paintToDo(newTodoObj); // 화면에 그 toDo를 그려줌
     saveToDos(); // 그리고 그 toDo를 저장함 
 }
 
@@ -44,8 +50,11 @@ const savedToDos = localStorage.getItem(TODOS_KEY)
 if(savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos); // loclaStorage에서 가져온 string이 JS object로 변하게 함 ex)"[a,b,c,d]" (string) => [a, b, c, d] (array);
     toDos = parsedToDos
-    parsedToDos.forEach(paintToDo);
+    parsedToDos.forEach(paintToDo); // forEach함수는 paintToDo를 parsedToDos 배열의 요소마다 실행함. 
 }
+
+
+
 
 /* 정리 
 local storage에 array로 저장이 안 되기 때문에 
