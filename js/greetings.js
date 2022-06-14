3f7760ba4add8db3d14e844c2671137c087d7a03
yeonkr <1714882@sookmyung.ac.kr>
2022-06-14 23:55:06 +0900

    7.5

--- js/greetings.js
+++ js/greetings.js
@@ -7,9 +7,10 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault(); 
-    loginForm.classList.add("HIDDEN_CLASSNAME"); 
+    loginForm.classList.add(HIDDEN_CLASSNAME); 
     const username = loginInput.value; 
-    localStorage.setItem("USERNAME_KEY", username); 
+    localStorage.setItem(USERNAME_KEY, username); 
+    paintGreetings(username);
 }
 
 function paintGreetings(username) { 
@@ -17,11 +18,13 @@ function paintGreetings(username) {
     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-const savedUsername = localStorage.getItem("USERNAME_KEY") 
+const savedUsername = localStorage.getItem(USERNAME_KEY) 
 
 if(savedUsername === null) {
+ // show the form
 loginForm.classList.remove(HIDDEN_CLASSNAME) 
 loginForm.addEventListener("submit", onLoginSubmit); 
 } else {
+    // show the greetings
     paintGreetings(savedUsername)
 }
